aca1246a25353d281d5139aaf31478da9fd6ba52
marialapc <marialopez.apc@gmail.com>
2022-12-12 17:38:46 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+
+import "../styles/App.scss";
+
+
+function App() {
+  // variables estado
+
+
+  // useEffect
+
+
+ 
+  // Funciones handler
+
+  // funciones render
+
+  //return
+
+  return(
+   <>
+ <h1>hi world</h1>
+
+  </>
+  );
+}
+
+export default App;
