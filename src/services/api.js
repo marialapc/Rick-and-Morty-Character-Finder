e08276e7a5dd86c1b39b7320280a5928ab9b0274
marialapc <marialopez.apc@gmail.com>
2022-12-12 17:59:04 +0100

    fetch and clean data

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const getDataFromApi = () => {
+  return fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((data) => {
+      const cleanData = data.results.map((character) => {
+        return {
+          id: character.id,
+          image: character.image,
+          name: character.name,
+          species: character.species,
+        };
+      });
+      return cleanData;
+    });
+};
+export default getDataFromApi;
